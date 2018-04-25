67a968ae77b9f2eac58b781d138374455851f253
Abhilash Bolla <abhilash.gis@gmail.com>
2018-04-25 14:41:46 +0530

    changed first name and last name to only name

--- main_app/models.py
+++ main_app/models.py
@@ -7,7 +7,6 @@ from django.db import models
 
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    name = models.CharField(max_length=60)
     email = models.CharField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=15, blank=True)
